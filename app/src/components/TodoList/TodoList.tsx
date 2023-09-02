a48d5934f4fde9f25a64888ac5e95b07fff04352
@vik_razrab <piton20091986@gmail.com>
2023-09-03 00:58:52 +0300

    finish development

--- app/src/components/TodoList/TodoList.tsx
+++ app/src/components/TodoList/TodoList.tsx
@@ -1,41 +1,80 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './TodoList.css'
 import http from '../../http-common'
 import add from './add_bold.png'
 import avatar from './Avatar.png'
+import { faker } from '@faker-js/faker';
+import moment from "moment";
 
 const TodoList = () => {
 
-    //const nameDuplication = await http.get('/api/get-user-by-name', { params: { name: formData.name } });
+    var startDate = faker.date.past();
+    var endDate = faker.date.future();
+    var formattedStartDate = moment(startDate).format('MMM D, h:mm a');
+    var formattedEndDate = moment(endDate).format('MMM D, h:mm a');
+    var randomDescription = faker.lorem.words(100)
+    var randomTagOne = faker.lorem.words(1)
+    var randomTagTwo = faker.lorem.words(1)
+
+    const[todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const getTodos = async() => {
+            const res = await http.get('/todos')
+            setTodos(res.data)
+        }
+        getTodos()
+    }, [])
+
+    const GenerateDataHandler = () => {
+        startDate = faker.date.past();
+        endDate = faker.date.future();
+        formattedStartDate = moment(startDate).format('MMM D, h:mm a');
+        formattedEndDate = moment(endDate).format('MMM D, h:mm a');
+        randomDescription = faker.lorem.words(100)
+        randomTagOne = faker.lorem.words(1)
+        randomTagTwo = faker.lorem.words(1)
+        return null
+    }
         
     return(
         <div className="TodoListWrapper">
-            <div className="card">
-                <div className="TopLine">
-                    <p className="green">Today</p>
-                    <div><img src={add} /><p>2</p></div>
-                </div>
-                <div className="Todo">
-                    <div className="TodoTitle">
-                        <input type="checkbox"></input>
-                        <h3>Title</h3>
-                    </div>
-                    <div className="TodoDate">
-                        <p className="green">09/23/2000</p>
-                        <p className="green">09/23/2000</p>
-                    </div>
-                    <div className="TodoDescription">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel quis odit, sunt pariatur aperiam cum. Minima sed placeat corrupti natus quis. Harum, provident minus? Recusandae iure quasi voluptas nostrum veritatis.</p>
-                    </div>
-                    <div className="BottomLine">
-                        <span>
-                            <a href="">Фиолетовый</a>
-                            <a href="">Серый</a>
-                        </span>
-                        <img src={avatar}></img>
-                    </div>
-                </div>
+            <div className="TopLine">
+                <p className="green main-font-weight">Today</p>
+                <div><img src={add} /><p className="grey">{todos.length}</p></div>
             </div>
+            {todos &&
+                todos.map((t, idx) => (
+                    <div className="card" key={idx}>
+                        {GenerateDataHandler()}
+                        <div className="Todo">
+                            <div className="TodoTitle">
+                                <input 
+                                type="checkbox"
+                                checked={t['completed']}
+                                readOnly
+                                />
+                                <h3 className="task-title">{t['title']}</h3>
+                            </div>
+                            <div className="TodoDate">
+                                <p className="green main-font-weight">{formattedStartDate}</p>
+                                <p className="green main-font-weight">{formattedEndDate}</p>
+                            </div>
+                            <div className="TodoDescription">
+                                <p className="ellipsis grey">{randomDescription}</p>
+                            </div>
+                            <div className="BottomLine">
+                                <span>
+                                    <a href="" className="main-font-weight">{randomTagOne}</a>
+                                    <a href="" className="main-font-weight grey">{randomTagTwo}</a>
+                                </span>
+                                <img src={avatar}></img>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                )
+            }
         </div>
     )
 }
